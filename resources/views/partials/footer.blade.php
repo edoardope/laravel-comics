935e8774fc585d70fe5f98c54d834601859f2915
edoardope <pencoedoardo@gmail.com>
2023-06-11 19:31:03 +0200

    progetto finito

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,46 @@
 <footer>
-    
-</footer>
+    <section id="b">
+      <div id="footerLink">
+        <span>
+          <h3>DC COMICS</h3>
+          <ul>
+            <li><a href="">link</a></li>
+            <li><a href="">link</a></li>
+            <li><a href="">link</a></li>
+            <li><a href="">link</a></li>
+            <li><a href="">link</a></li>
+            <li><a href="">link</a></li>
+          </ul>
+        </span>
+        <span class="footerColumn">
+          <h3>DC COMICS</h3>
+          <ul>
+            <li><a href="">link</a></li>
+            <li><a href="">link</a></li>
+            <li><a href="">link</a></li>
+            <li><a href="">link</a></li>
+            <li><a href="">link</a></li>
+            <li><a href="">link</a></li>
+          </ul>
+        </span>
+      </div>
+      <div id="footerLogo">
+
+      </div>
+    </section>
+    <section id="c">
+      <div>
+        <span id="d">
+          <a href="">SING-UP NOW!</a>
+        </span>
+      </div>
+      <div>
+        <h2>FOLLOW US</h2>
+        <img src="./assets/footer-facebook.png" alt="">
+        <img src="./assets/footer-twitter.png" alt="">
+        <img src="./assets/footer-youtube.png" alt="">
+        <img src="./assets/footer-pinterest.png" alt="">
+        <img src="./assets/footer-periscope.png" alt="">
+      </div>
+    </section>
+  </footer>
