1d271fb13c2816ff922c2b3df9d343f3763169eb
edoardope <pencoedoardo@gmail.com>
2023-06-11 18:37:39 +0200

    installation + layout

---
+++ resources/views/pages/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+
+@section ('title')
+    laravel comics | home
+@endsection
+
+@section('content')
+    <h1>home page</h1>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<nav>
+    <ul>
+        <li>
+            <a href="{{ route('home') }}">home</a>
+        </li>
+        <li>
+            <a href="">fumetti</a>
+        </li>
+    </ul>
+</nav>
